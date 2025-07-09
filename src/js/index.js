3e683f283a1dcd81969bf7e4ef71a489308d1315
Gabriel Ribeiro <gabrielribeirotnc@gmail.com>
2025-07-09 21:52:03 +0000

    fix: function showPokemon validation

--- src/js/index.js
+++ src/js/index.js
@@ -18,25 +18,34 @@ const showPokemon = async (pokemon) => {
   const showId = document.querySelector(".showId");
   showId.innerHTML = `${data.id} - `;
 
-  const imagePokemon = document.querySelector('.imagePokemon')
+  const imagePokemon = document.querySelector(".imagePokemon");
   const {
     other: {
       home: { front_default },
     },
   } = await data.sprites;
   imagePokemon.src = front_default;
-
-  
 };
-showPokemon('1');
+
+showPokemon("1");
 
 const btnSearch = document.querySelector(".btnSearch");
 btnSearch.addEventListener("click", (e) => {
-  e.preventDefault()
-  const pokemon = namePokemon.value.toString();
-    showPokemon(pokemon);
+  e.preventDefault();
+  const pokemon = namePokemon.value.toLowerCase().trim();
+
+  const isNumber = !isNaN(pokemon);
+  const num = Number(pokemon);
+
+  if ((isNumber && (num <= 0 || num > 1025)) || pokemon === "") {
+    alert("Insira um nome ou número válido de Pokémon.");
+    return;
+  }
+  try{
+  showPokemon(pokemon);
+  }
+
+  catch (error){
+    alert("Pokémon não encontrado.");
+  }
 });
-
-
-
-

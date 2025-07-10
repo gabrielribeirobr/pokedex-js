07a719f715e69ec46e0465bfbb7b7c7e21fdc106
Gabriel Ribeiro <gabrielribeirotnc@gmail.com>
2025-07-10 17:56:07 +0000

    feat: created function to next and prev Pokemon and validation fecth

--- src/js/index.js
+++ src/js/index.js
@@ -2,21 +2,40 @@ const showName = document.querySelector(".showName");
 const namePokemon = document.getElementById("namePokemon");
 const boxImage = document.querySelector(".boxImage");
 const BASE_URL = "https://pokeapi.co/api/v2";
+const btnNext = document.querySelector('.nextBtn');
+const btnPrev = document.querySelector('.prevBtn');
+
+let currentId = 1;
 
 const getPokeApi = async (pokemon) => {
-  const response = await fetch(`${BASE_URL}/pokemon/${pokemon}`);
-  const data = await response.json();
-  console.log(data);
 
-  return data;
+  const response = await fetch(`${BASE_URL}/pokemon/${pokemon}`);
+  
+  
+  if(!response.ok){
+    alert('Pokémon inválido');
+    return null;
+  }  else {
+    const data = await response.json();
+    console.log(data);
+    return data;
+  }
+  
+
+  
 };
 
 const showPokemon = async (pokemon) => {
   const data = await getPokeApi(pokemon);
+
+  if(!data) return;
+
+  currentId = data.id;
+
   showName.innerHTML = data.name;
 
   const showId = document.querySelector(".showId");
-  showId.innerHTML = `${data.id} - `;
+  showId.innerHTML = `${currentId} - `;
 
   const imagePokemon = document.querySelector(".imagePokemon");
   const {
@@ -25,12 +44,35 @@ const showPokemon = async (pokemon) => {
     },
   } = await data.sprites;
   imagePokemon.src = front_default;
-};
 
+};
 showPokemon("1");
 
+const nextPokemon = () => {
+  const nextId = currentId + 1;
+
+  if (nextId <= 1025) {
+    showPokemon(nextId);
+  } else {
+    alert("Esse é o último Pokémon disponível.");
+  }
+};
+
+const prevPokemon = () => {
+  const prevId = currentId - 1;
+
+  if (prevId > 0 ) {
+    showPokemon(prevId);
+  } else {
+    alert("Esse é o último Pokémon disponível.");
+  }
+};
+
+btnNext.addEventListener("click", nextPokemon);
+btnPrev.addEventListener("click", prevPokemon);
+
 const btnSearch = document.querySelector(".btnSearch");
-btnSearch.addEventListener("click", (e) => {
+btnSearch.addEventListener("click", async (e) => {
   e.preventDefault();
   const pokemon = namePokemon.value.toLowerCase().trim();
 
@@ -42,10 +84,14 @@ btnSearch.addEventListener("click", (e) => {
     return;
   }
   try{
-  showPokemon(pokemon);
+   await showPokemon(pokemon);
   }
 
   catch (error){
     alert("Pokémon não encontrado.");
   }
 });
+
+isNaN()
+
+
